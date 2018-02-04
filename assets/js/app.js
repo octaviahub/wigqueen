39cf5eaa774ca8424148e5b05f490e6359d69e63
Octavia Cephas <octavia.cephas@gmail.com>
2018-02-04 18:44:44 -0500

    carousel edits. not yet functional

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-	$('.carousel').carousel({
-	interval: 2000
-})	
+$(document).ready(function() {
+	require ('javascript.util');
+	const ArrayList = javascript.util.ArrayList;
+	const array = new ArrayList();
+	array.add(1);
+
 });
 
+$('#carouselExampleIndicators').on('slid.bs.carousel', function (e) {
+  $('#carouselExampleIndicators').carousel('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
+})
+
+$('#carouselExampleIndicators').carousel('1') // Will start sliding to the slide 1 and returns to the caller
+$('#carouselExampleIndicators').carousel('2') // !! Wil
